c11636679c58c40100cb65898a445dd1d4c0749b
johein93 <johein.pgr@gmail.com>
2015-10-24 13:37:50 +0630

    remaining on save file to disk

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Dependencies
+var fs = require('fs');
+
+
 $.fn.pureRGB = function(){
 	return this.css("backgroundImage").replace("linear-gradient","").split("),")[1].replace("))",")");
 }
@@ -20,6 +24,12 @@ var $canvas = $('canvas');
 var context = $canvas[0].getContext("2d");
 var lastEvent;
 var mouseDown = false;
+var burshSize = 1;
+var photoData = null;
+
+$canvas[0].width = parseInt($canvas.css('width'));
+$canvas[0].height = parseInt($canvas.css('height'));
+
 
 $("#availableColors").on("click","li a",function(){
 	$(this).parent("li").addClass("selected").siblings().removeClass("selected");
@@ -51,6 +61,50 @@ $("#chooseColor button").click(function(){
 
 
 
+$("#menu #left a").click(function(e){
+	var crase = e.currentTarget.className;
+	switch(crase){
+		case "decrase":
+		burshSize = (burshSize != 1) ? burshSize-1 : 1;
+		break;
+
+		case "incrase":
+		burshSize = (burshSize != 7) ? burshSize+1 : 7;
+		break;
+	}
+})
+
+
+function bindSavingToDisk () {
+    var $saver  = $('#saver');
+    $saver.change(function(){
+        var filePath = this.value;
+        fs.writeFile(filePath, photoData, 'base64', function (err) {
+            if (err) { alert('There was an error saving the photo:',err.message); }
+            photoData = null;
+        });
+    });
+}
+bindSavingToDisk();
+
+$("#menu #right a").click(function(e){
+	var action = e.currentTarget.className;
+	switch(action){
+		case "trash":
+		context.clearRect(0,0,$canvas[0].width,$canvas[0].height);
+		break;
+		case "save":
+		var $saver = $("#saver");
+		$saver.attr('nwsaveas','FunDrawImage.png');
+        photoData = $canvas[0].toDataURL('image/png').replace(/^data:image\/(png|jpg|jpeg);base64,/, '');
+		$saver.click();
+		break;
+	}
+})
+
+
+
+
 
 $canvas.mousedown(function(e){
 	lastEvent = e;
@@ -61,6 +115,7 @@ $canvas.mousedown(function(e){
 		context.beginPath();
 		context.moveTo(lastEvent.offsetX,lastEvent.offsetY);
 		context.lineTo(e.offsetX,e.offsetY);
+		context.lineWidth = burshSize;
 		context.strokeStyle = color;
 		context.stroke();
 		lastEvent = e;
@@ -70,16 +125,3 @@ $canvas.mousedown(function(e){
 }).mouseleave(function(){
 	$canvas.mouseup();
 });
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> e6dd7cb39ebd46a33978b65770664f6a9a4562e7
